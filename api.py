f852580184ef98b58b56bac667aa33483c4ffd33
jarry-xiao <jarry.xiao@gmail.com>
2021-06-28 16:59:55 -0500

    wrote a sketchy outline of what v1 might look like

--- api.py
+++ api.py
@@ -1,6 +1,19 @@
+from enum import Enum
 import json
 import requests
-import solana
+from requests.api import head
+import solana.public_key import PublicKey 
+import solana.transaction import Transaction
+import solana.account import Account 
+import solana.system_program import transfer, TransferParams 
+from spl.token.instructions import (
+    mint_to, MintToParams, transfer as spl_transfer
+)
+import base58
+from nacl.public import PrivateKey
+from nacl import signing
+
+Status = Enum("Success", "Failure")
 
 def deploy(self, network, contract, name, symbol):
     """
@@ -10,6 +23,7 @@ def deploy(self, network, contract, name, symbol):
     status = None
     address = None
     tx = None
+    # TODO
     # Perform blockchain operations
     return json.dumps(
         {
@@ -21,13 +35,12 @@ def deploy(self, network, contract, name, symbol):
 
 def wallet(self, network):
     """ Generate a wallet on the specified network and return the address and private key. """
-    address = None
-    private_key = None
-    # Perform blockchain operations
+    private_key = PrivateKey.generate()
+    address = base58.b58encode(signing.SigningKey(private_key.encode()).verify_key.encode())
     return json.dumps(
         {
             'address': address,
-            'private_key': private_key
+            'private_key': private_key.encode()
         }
     )
 
@@ -35,15 +48,30 @@ def topup(self, network, to, amount):
     """
     Send a small amount of native currency to the specified wallet to handle gas fees. Return a status flag of success or fail and the native transaction data.
     """
-    status = None
-    tx = None
-    # Perform blockchain operations
-    return json.dumps(
-        {
-            'status': status,
-            'tx': tx
-        }
-    )
+    try:
+        client = Client("https://api.mainnet-beta.solana.com/")
+        sender = Account(SOURCE_ACCOUNT_KEY) # Global variable from config file
+        receiver = Account(to)
+        signers = [sender, receiver]
+        ix = transfer(TransferParams(from_pubkey=sender.public_key(), to_pubkey=receiver.public_key(), lamports=amount))
+        tx = Transaction().add(ix, *signers)
+        response = client.send_transaction(tx).json()
+        if "error" not in response:
+            return json.dumps(
+                {
+                    'status': Status.Success,
+                    'tx': response['result']
+                }
+            )
+    except:
+        pass
+    finally:
+        return json.dumps(
+            {
+                'status': Status.Failure,
+                'tx': tx
+            }
+        )
 
 def mint(self, network, contract, address, batch, sequence, limit, name, description, link, created, content='', **kw):
     """
@@ -56,31 +84,75 @@ def mint(self, network, contract, address, batch, sequence, limit, name, descrip
     These are text fields intended to be written directly to the blockchain. created is an ISO standard timestamp string (UTC)
     content is an optional JSON string for customer-specific data.
     Return a status flag of success or fail and the native transaction data.
+
     """
-    status = None
     tx = None
-    # Perform blockchain operations
-    return json.dumps(
-        {
-            'status': status,
-            'tx': tx
-        }
-    )
+    try:
+        client = Client("https://api.mainnet-beta.solana.com/")
+        signers = [Account(SOURCE_ACCOUNT_KEY)]
+        mint_to_params = MintToParams(
+            program_id=PublicKey(TOKEN_PROGRAM_ID),
+            mint=PublicKey(MINT_ACCOUNT_KEY),
+            dest=PublicKey(address),
+            owner=PublicKey(SOURCE_ACCOUNT_PUBKEY),
+            signers=signers,
+        )
+        ix = mint_to(mint_to_params)
+        tx = client.set_transaction(ix, *signers) 
+        response = client.send_transaction(tx).json()
+        if "error" not in response:
+            return json.dumps(
+                {
+                    'status': Status.Success,
+                    'tx': tx,
+                }
+            )
+    except:
+        pass
+    finally:
+        return json.dumps(
+            {
+                'status': Status.Failure,
+                'tx': tx
+            }
+        )
 
 def send(self, network, contract, sender, to, token, private_key, contract_address):
     """
     Transfer a token on a given network and contract from the sender to the recipient.
     May require a private key, if so this will be provided encrypted using Fernet: https://cryptography.io/en/latest/fernet/
-    Return a status flag of success or fail and the native transaction data. """
-    status = None
+    Return a status flag of success or fail and the native transaction data. 
+    """
     tx = None
-    # Perform blockchain operations
-    return json.dumps(
-        {
-            'status': status,
-            'tx': tx
-        }
-    )
+    try:
+        client = Client(network)
+        signers = [Account(SOURCE_ACCOUNT_KEY), Account(private_key)]
+        spl_transfer_params = TransferParams(
+            program_id=Pubkey(TOKEN_PROGRAM_ID),
+            source=Pubkey(sender),
+            dest=Pubkey(to),
+            owner=Pubkey(SOURCE_ACCOUNT_PUBKEY),
+            signers=signers,
+        )
+        ix = spl_transfer(spl_transfer_params)
+        tx = client.set_transaction(ix, *signers) 
+        response = client.send_transaction(tx).json()
+        if "error" not in response:
+            return json.dumps(
+                {
+                    'status': Status.Success,
+                    'tx': tx,
+                }
+            )
+    except:
+        pass
+    finally:
+        return json.dumps(
+            {
+                'status': Status.Failure,
+                'tx': tx
+            }
+        )
 
 def burn(self, network, contract, sender, token, private_key, contract_address):
     """
@@ -88,12 +160,34 @@ def burn(self, network, contract, sender, token, private_key, contract_address):
     May require a private key, if so this will be provided encrypted using Fernet: https://cryptography.io/en/latest/fernet/
     Return a status flag of success or fail and the native transaction data.
     """
-    status = None
     tx = None
-    # Perform blockchain operations
-    return json.dumps(
-        {
-            'status': status,
-            'tx': tx
-        }
-    )
+    try:
+        client = Client(network)
+        signers = [Account(SOURCE_ACCOUNT_KEY), Account(private_key)]
+        burn_params = BurnParams(
+            program_id=Pubkey(TOKEN_PROGRAM_ID),
+            account=Pubkey(sender),
+            mint=PublicKey(MINT_ACCOUNT_KEY),
+            owner=Pubkey(SOURCE_ACCOUNT_PUBKEY),
+            amount=1,
+            signers=signers,
+        )
+        ix = spl_transfer(burn_params)
+        tx = client.set_transaction(ix, *signers) 
+        response = client.send_transaction(tx).json()
+        if "error" not in response:
+            return json.dumps(
+                {
+                    'status': Status.Success,
+                    'tx': tx,
+                }
+            )
+    except:
+        pass
+    finally:
+        return json.dumps(
+            {
+                'status': Status.Failure,
+                'tx': tx
+            }
+        )
