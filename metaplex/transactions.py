ed830a50b8bd50d23b135d4ad115a8311911f2f5
Alice <alize_in_cryptoland@protonmail.com>
2021-09-13 16:03:26 +0200

    Add function to update token metadata

--- metaplex/transactions.py
+++ metaplex/transactions.py
@@ -108,6 +108,31 @@ def topup(api_endpoint, sender_account, to, amount=None):
     return tx, signers
 
 
+def update_token_metadata(api_endpoint, source_account, mint_token_id, link, data, creators_addresses, creators_verified, creators_share):
+    """
+    Updates the json metadata for a given mint token id.
+    """
+    mint_account = PublicKey(mint_token_id)
+    signers = [source_account]
+
+    tx = Transaction()
+    update_metadata_data = update_metadata_instruction_data(
+        data['name'],
+        data['symbol'],
+        link,
+        creators_addresses,
+        creators_verified,
+        creators_share,
+    )
+    update_metadata_ix = update_metadata_instruction(
+        update_metadata_data,
+        source_account.public_key(),
+        mint_account,
+    )
+    tx = tx.add(update_metadata_ix) 
+    return tx, signers
+
+
 def mint(api_endpoint, source_account, contract_key, dest_key, link, supply=1):
     """
     Mint a token on the specified network and contract, into the wallet specified by address.
